9ea18dfc28077ac1b3c2615caa773c045c9905a3
erikanderson <edawebdesign@gmail.com>
2015-11-08 15:51:55 -0700

    Accordian

--- public/app/controllers/reportController.js
+++ public/app/controllers/reportController.js
@@ -6,18 +6,18 @@ app.controller('reportController', function($scope, nodeService, report){
 
   $scope.oneAtATime = true;
 
-  $scope.groups = [
-    {
-      title: 'Dynamic Group Header - 1',
-      content: 'Dynamic Group Body - 1'
-    },
-    {
-      title: 'Dynamic Group Header - 2',
-      content: 'Dynamic Group Body - 2'
-    }
-  ];
+  // $scope.groups = [
+  //   {
+  //     title: 'Dynamic Group Header - 1',
+  //     content: 'Dynamic Group Body - 1'
+  //   },
+  //   {
+  //     title: 'Dynamic Group Header - 2',
+  //     content: 'Dynamic Group Body - 2'
+  //   }
+  // ];
 
-  $scope.items = $scope.report.resource_events.data;
+  // $scope.items = $scope.report.resource_events.data;
 
   $scope.status = {
     isFirstOpen: true,
--- public/app/services/nodeService.js
+++ public/app/services/nodeService.js
@@ -22,6 +22,16 @@ app.service('nodeService', function($http, $q){
     }).then(function(res){
         console.log('res: ', res);
         console.log('res.data: ', res.data);
+        for (var i = 0; i < res.data[0].resource_events.data.length; i++) {
+          console.log('in if: ', res.data[0].resource_events.data[i]);
+          if (res.data[0].resource_events.data[i].status == 'success'){
+            res.data[0].resource_events.data[i].panel_status = 'panel-success';
+          }
+          if (res.data[0].resource_events.data[i].status == 'fail'){
+            res.data[0].resource_events.data[i].panel_status = 'panel-danger';
+          }
+
+        }
         deferred.resolve(res.data[0]);
     })
     return deferred.promise;
